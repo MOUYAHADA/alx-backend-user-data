00524d575fb74987e3429472bf2c40665c755efb
Mohamed Mouyahada <mouyahada@gmail.com>
2024-11-13 20:35:56 +0100

    auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,23 +26,19 @@ if AUTH_TYPE == 'auth':
 @app.before_request
 def load_auth():
     """loads auth before request"""
-    if auth is None:
-        return
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+        ]
 
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-    ]
+        if auth.require_auth(request.path, excluded_paths) is True:
+            if auth.authorization_header(request) is None:
+                abort(401, description='Unauthorized')
 
-    if auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
+            if auth.current_user(request) is None:
+                abort(403, description='Forbidden')
 
 
 @app.errorhandler(404)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,8 +20,8 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
         if request:
-            if request.get('Authorization'):
-                return request.get('Authorization')
+            if request.headers.get('Authorization', None):
+                return request.headers.get('Authorization')
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
