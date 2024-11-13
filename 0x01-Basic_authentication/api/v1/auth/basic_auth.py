45f18a6ea2232754a08c493be06aa4c1febf53c9
Mohamed Mouyahada <mouyahada@gmail.com>
2024-11-14 00:24:51 +0100

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """BasicAuth that inherits from Auth"""
 
+from base64 import b64decode
 from api.v1.auth.auth import Auth
 
 
@@ -19,3 +20,19 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.replace('Basic ', '').strip()
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64
+        string base64_authorization_header"""
+        if (base64_authorization_header is None)\
+                or (type(base64_authorization_header) is not str):
+            return None
+
+        try:
+            header = b64decode(base64_authorization_header.encode('utf-8'))
+        except Exception:
+            return None
+
+        return header.decode('utf-8')
