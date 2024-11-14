0087d4adc52beb04a218f7da3d9dac40b695bc5b
Mohamed Mouyahada <mouyahada@gmail.com>
2024-11-14 17:44:50 +0100

    user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,20 +54,22 @@ class BasicAuth(Auth):
         email, password = d.split(":")
         return email, password
 
-    def user_object_from_credentials(self,
-                                     user_email: str,
+    def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
         """returns the User instance based on
         his email and password."""
-        if user_email and user_email and\
-                type(user_email) is str and type(user_pwd) is str:
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
 
-            if User.count():
-                result = User.search({'email': user_email})
-
-                if len(result):
-                    user = result[0]
-                    if User.is_valid_password(user, user_pwd):
-                        return user
-
-        return None
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
