60765422c44f26a9f28b91473829ba885a32d508
Mohamed Mouyahada <mouyahada@gmail.com>
2024-11-14 18:25:42 +0100

    current user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -73,3 +73,22 @@ class BasicAuth(Auth):
             return None
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """overloads Auth and retrieves
+        the User instance for a request_
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is not None:
+            token = self.extract_base64_authorization_header(auth_header)
+            if token is not None:
+                decoded = self.decode_base64_authorization_header(token)
+                if decoded is not None:
+                    email, password = self.extract_user_credentials(decoded)
+                    if email is not None:
+                        user = self.user_object_from_credentials(
+                            email, password)
+                        if user is not None:
+                            return user
+
+        return
