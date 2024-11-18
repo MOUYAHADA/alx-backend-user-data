9e272748ebfe8f67014f6d4d81a6eaac0b3e89b5
Mohamed Mouyahada <mouyahada@gmail.com>
2024-11-18 01:31:01 +0100

    user by session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,12 @@
 """
 SessionAuth that inherits from Auth
 """
+from typing import TypeVar
 from uuid import uuid4
 
 from requests import session
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -26,3 +28,12 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return __class__.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns a User instance based on a cookie value"""
+        cookie = self.session_cookie(request)
+
+        if cookie:
+            user_id = self.user_id_for_session_id(cookie)
+            if user_id:
+                return User.get(user_id)
