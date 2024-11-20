dac32beb0e9b881be46b007b4849d1e2d751988c
Mohamed Mouyahada <mouyahada@gmail.com>
2024-11-20 02:53:10 +0100

    log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -26,6 +26,25 @@ def users():
 
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def sessions():
+    """Creates a new session for user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email and password:
+
+        if AUTH.valid_login(email, password):
+
+            session_id = AUTH.create_session(email)
+            res = jsonify({"email": email, "message": "logged in"})
+            res.set_cookie('session_id', session_id)
+
+            return res
+
+    abort(401)
 
 
 if __name__ == '__main__':
