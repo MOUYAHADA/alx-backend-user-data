2734156583496f353e712c8ab005c467210bcda3
Mohamed Mouyahada <mouyahada@gmail.com>
2024-11-20 03:03:15 +0100

    destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,3 +81,11 @@ class Auth:
                 return user
             except NoResultFound:
                 return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Updates the corresponding user’s session ID to None"""
+        if type(user_id) is int:
+            try:
+                user = self._db.update_user(user_id=user_id, session_id=None)
+            except ValueError:
+                pass
